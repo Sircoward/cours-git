f3d0de16ea5615d519aa98794873c52831dfd70e
Sircoward <misterballians@gmail.com>
2022-09-23 09:48:36 +0200

    kek

--- logs.js
+++ logs.js
@@ -2,12 +2,9 @@
 function log(message){
     console.log(message);
 }
-
-<<<<<<< HEAD
 function tonpere (){
     console.log('sheeeeeeeeeeeeeesh');
-=======
+}
 function warn(message){
     console.warn(message);
->>>>>>> refs/remotes/origin/master
 }
